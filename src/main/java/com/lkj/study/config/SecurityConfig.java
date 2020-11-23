b03e452d06c9ec75dbbbeab1785f5118c05b3388
이경진 <bramhome5848@gmail.com>
2020-11-23 19:40:38 +0900

    20201123 Account Controller & Test, Security Config

---
+++ src/main/java/com/lkj/study/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.lkj.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity  //security 설정을 직접 하게 됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //특정 요청들을 security check 하지 않도록 설정
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "sign-up", "/check-email", "check-email_token",
+                        "/email-login", "/check-email-login", "/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();  //나머지는 security check
+    }
+}
---
+++ src/test/java/com/lkj/study/account/AccountControllerTest.java
@@ -0,0 +1,31 @@
+package com.lkj.study.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면 보이는지 확인 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        //SecurityConfig 에서 sign-up을 제외시키지 않으면 테스트는 실패하게 됨
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print()) //view code 확인
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+}
