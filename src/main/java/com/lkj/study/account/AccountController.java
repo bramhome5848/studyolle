141060d6e5da3f23dc16f0db107aa86a586fda69
이경진 <bramhome5848@gmail.com>
2020-11-24 11:48:32 +0900

    20201124 Handling sign up form submit

--- src/main/java/com/lkj/study/account/AccountController.java
+++ src/main/java/com/lkj/study/account/AccountController.java
@@ -1,16 +1,66 @@
 package com.lkj.study.account;
 
+import com.lkj.study.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    @InitBinder("signUpForm")   //signUpForm data를 받을 때 사용할 binder 를 설정 가능
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String singUpForm(Model model) {
-        model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(/*"signUpForm",*/ new SignUpForm()); //camel case 이름이 같으면 "signUpForm" 생략 가능
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid /* @ModelAttribute */ SignUpForm signUpForm, Errors errors) {  //@ModelAttribute 생략 가능
+        if(errors.hasErrors()) {    //valid error 에 걸릴 경우
+            return "account/sign-up";
+        }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO encoding 필요
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+
+        //메일 보내기
+        newAccount.generateEmailCheckToken();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디 올래, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=slsdkfjlskdfjlksdjflk" + newAccount.getEmailCheckToken() +
+                "&email=" + newAccount.getEmail());
+
+        javaMailSender.send(mailMessage);
+        return "redirect:/";
+    }
 }
