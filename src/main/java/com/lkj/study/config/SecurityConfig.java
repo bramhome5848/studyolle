bdb78d71fb89c7e9784efcd296dbd7d1e0dddffe
이경진 <bramhome5848@gmail.com>
2020-11-30 14:26:49 +0900

    20201130 Remember me

--- src/main/java/com/lkj/study/config/SecurityConfig.java
+++ src/main/java/com/lkj/study/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.lkj.study.config;
 
+import com.lkj.study.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity  //security 설정을 직접 하게 됨
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountService accountService;
+    private final DataSource dataSource;    //jpa 사용하고 있기 때문에 bean 등록 되어 있음
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,6 +39,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //logout
         http.logout()
                 .logoutSuccessUrl("/"); //logout 했을 때 어디로 갈지
+
+        /**
+         * 1. spring security 해시 기반의 쿠키
+         * username, password, 만료기간, key(애플리케이션 마다 다른 값)
+         * 치명적인 단점 - 쿠키를 다른 사람이 가져가면 그 계정은 탈취당한 것 -> 쿠키를 통해 비밀번호 변경해버림...
+         *
+         * 2. 개선한 방법
+         * username, 토큰(랜덤, 매번바뀜), 시리즈(랜덤, 고정)
+         * 쿠키를 탈취 당한 경우, 희생자는 유효하지 않은 토큰과 유효한 시리즈와 username 으로 접속 에
+         * -> 탈취 당한 경우 이미 새로운 토큰으 갱신되었기 때문 기존의 토큰은 유효하지 않음
+         * 이 경우, 모든 토큰을 삭제하여 해커가 더이상 탈취한 쿠키를 사용하지 못하도록 방지 -> login 을 다시 하도록 유도
+         */
+        //remember me
+        //1. 해시 기반의 쿠키
+//        http.rememberMe()
+//                .key("sdfsdfsdf");
+
+        //2. 개선 방법
+        http.rememberMe()
+                .userDetailsService(accountService)  //userDetail -> accountService 에서 구현했기 때문에 사용가능
+                .tokenRepository(tokenRepository());
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        //jdbc 기반의 토큰 repository 구현체
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Override
---
+++ src/main/java/com/lkj/study/domain/PersistentLogins.java
@@ -0,0 +1,35 @@
+package com.lkj.study.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+/**
+ * jdbcTokenRepositoryImpl 이 사용하는 table 이 존재해야함
+ * jpa 를 사용하고 있고 현재 In memory db 를 사용하기 때문에 entity 정보를 보고 테이블을 만들어줌
+ * 따라서 jdbcTokenRepositoryImpl 이 사용하는 table 스키마에 해당하는 테이블이 생성될 수 있도록 entity 추가
+ */
+@Table(name = "persistent_logins")
+@Entity
+@Getter @Setter
+public class PersistentLogins {
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false, length = 64)
+    private LocalDateTime lastUsed;
+}
