220fce9468e5aae3373e3361394f1be6faa66835
이경진 <bramhome5848@gmail.com>
2020-12-03 19:15:18 +0900

    20201203 Zone domain & Init data

---
+++ src/main/java/com/lkj/study/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.lkj.study.zone;
+
+import com.lkj.study.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
