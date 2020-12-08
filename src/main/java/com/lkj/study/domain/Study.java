ca10b89f63f7d0ad317b9431797f897b3b900964
이경진 <bramhome5848@gmail.com>
2020-12-08 15:34:28 +0900

    20201208 View Study

--- src/main/java/com/lkj/study/domain/Study.java
+++ src/main/java/com/lkj/study/domain/Study.java
@@ -1,5 +1,6 @@
 package com.lkj.study.domain;
 
+import com.lkj.study.account.UserAccount;
 import lombok.*;
 
 import javax.persistence.*;
@@ -7,6 +8,16 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * study 뷰를 변경할 때 마다 발생하는 쿼리 - 여러번
+ * 조회할 데이터라면 쿼리 개수를 줄이고 Join 을 통해서 가져오자
+ * Left outer join 으로 연관 데이터를 한번에 조회할 수도 있음
+ */
+@NamedEntityGraph(name = "Study.withAll", attributeNodes = {
+        @NamedAttributeNode("tags"),
+        @NamedAttributeNode("zones"),
+        @NamedAttributeNode("managers"),
+        @NamedAttributeNode("members")})
 @Entity
 @Getter
 @Setter
@@ -61,4 +72,21 @@ public class Study {
     public void addManager(Account account) {
         this.managers.add(account);
     }
+
+    //html 파일에서 method 직접 호출이 가능
+    public boolean isJoinable(UserAccount userAccount) {
+        Account account = userAccount.getAccount();
+        return this.isPublished() && this.isRecruiting()
+                && !this.members.contains(account) && !this.managers.contains(account);
+
+    }
+
+    public boolean isMember(UserAccount userAccount) {
+        return this.members.contains(userAccount.getAccount());
+    }
+
+    public boolean isManager(UserAccount userAccount) {
+        return this.managers.contains(userAccount.getAccount());
+    }
+
 }
--- src/main/java/com/lkj/study/study/StudyRepository.java
+++ src/main/java/com/lkj/study/study/StudyRepository.java
@@ -1,8 +1,13 @@
 package com.lkj.study.study;
 
 import com.lkj.study.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
     boolean existsByPath(String path);
+
+    @EntityGraph(value = "Study.withAll", type = EntityGraph.EntityGraphType.LOAD)
+    Study findByPath(String path);
 }
