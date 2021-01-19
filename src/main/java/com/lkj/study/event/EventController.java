22e6c177119f63c7f3f772125485454ecb518030
이경진 <bramhome5848@gmail.com>
2021-01-19 18:16:43 +0900

    20210119 Event create

---
+++ src/main/java/com/lkj/study/event/EventController.java
@@ -0,0 +1,57 @@
+package com.lkj.study.event;
+
+import com.lkj.study.account.CurrentAccount;
+import com.lkj.study.domain.Account;
+import com.lkj.study.domain.Event;
+import com.lkj.study.domain.Study;
+import com.lkj.study.event.form.EventForm;
+import com.lkj.study.event.validator.EventValidator;
+import com.lkj.study.study.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/study/{path}")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final StudyService studyService;
+    private final EventService eventService;
+    private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
+
+    @InitBinder("eventForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(eventValidator);
+    }
+
+    @GetMapping("/new-event")
+    public String newEventForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        model.addAttribute(study);
+        model.addAttribute(account);
+        model.addAttribute(new EventForm());
+        return "event/form";
+    }
+
+    @PostMapping("/new-event")
+    public String newEventSubmit(@CurrentAccount Account account, @PathVariable String path,
+                                 @Valid EventForm eventForm, Errors errors, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            model.addAttribute(study);
+            return "event/form";
+        }
+
+        Event event = eventService.createEvent(modelMapper.map(eventForm, Event.class), study, account);
+        return "redirect:/study/" + study.getEncodedPath() + "/events/" + event.getId();
+    }
+}
---
+++ src/main/java/com/lkj/study/event/EventService.java
@@ -0,0 +1,24 @@
+package com.lkj.study.event;
+
+import com.lkj.study.domain.Account;
+import com.lkj.study.domain.Event;
+import com.lkj.study.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    public Event createEvent(Event event, Study study, Account account) {
+        event.setCreateBy(account);
+        event.setCreatedDateTime(LocalDateTime.now());
+        event.setStudy(study);
+        return eventRepository.save(event);
+    }
+}
