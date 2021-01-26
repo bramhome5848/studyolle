f7c0249423c78f62d8d96bc39c9ff37fa8e038e1
이경진 <bramhome5848@gmail.com>
2021-01-26 20:42:08 +0900

    20210126 Notification icon

--- src/main/java/com/lkj/study/modules/notification/NotificationRepository.java
+++ src/main/java/com/lkj/study/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.lkj.study.modules.notification;
 
+import com.lkj.study.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
